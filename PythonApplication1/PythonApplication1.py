57f0ee60b6a8219e19b122a936755e822983a008
ZhangXin <18810011293@163.com>
2021-05-13 22:31:31 +0800

    建立基本框架，啥也没干

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,16 +1,17 @@
-import array
+from PythonApplication1 import numpy
 
 class NeuralNetWork:
 
     #初始化函数
     def __init__(self, input_nodes, hidden_nodes, output_nodes, learning_rate):
 
+        #初始化输入层、隐藏层、输出层节点、训练速率
         self.in_nodes = input_nodes
         self.hn_nodes = hidden_nodes
         self.ot_nodes = output_nodes
         self.lr = learning_rate
         pass
-
+    
     #训练函数
     def train():
         pass
