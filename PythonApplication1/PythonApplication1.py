5e03cd8ecaf64a6260093484c1f0e34426fb36be
ZhangXin <18810011293@163.com>
2021-05-13 21:55:30 +0800

    完善初始化函数，主要是各个参数赋值

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -3,7 +3,12 @@ import array
 class NeuralNetWork:
 
     #初始化函数
-    def __init__():
+    def __init__(self, input_nodes, hidden_nodes, output_nodes, learning_rate):
+
+        self.in_nodes = input_nodes
+        self.hn_nodes = hidden_nodes
+        self.ot_nodes = output_nodes
+        self.lr = learning_rate
         pass
 
     #训练函数
