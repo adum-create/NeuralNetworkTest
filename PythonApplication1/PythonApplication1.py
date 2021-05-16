8e15ca6e50974ca8c10319aa8e1bbdb8bc6f8e5e
ZhangXin <18810011293@163.com>
2021-05-16 11:02:53 +0800

    添加测试部分和训练部分，但是数据读取在其他位置

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,5 +1,6 @@
 import numpy
 import scipy.special
+import matplotlib.pyplot
 
 class NeuralNetWork:
 
@@ -18,7 +19,6 @@ class NeuralNetWork:
 
         #激活函数
         self.active_function = lambda x: scipy.special.expit(x);
-        pass
     
     #训练函数
     def train(self, input_list, target_list):
@@ -41,7 +41,6 @@ class NeuralNetWork:
         self.who += self.lr * numpy.dot((output_errors * final_outputs * (1-final_outputs)), numpy.transpose(hidden_outputs))
         self.wih += self.lr * numpy.dot((hidden_errors * hidden_outputs * (1-hidden_outputs)), numpy.transpose(inputs))
 
-        pass
     
     #查询函数
     def query(self, input_list):
@@ -60,13 +59,61 @@ class NeuralNetWork:
 
 
 #定义网络各层节点
-input_nodes = 3
-hidden_nodes = 3
-output_nodes = 3
+input_nodes = 784
+hidden_nodes = 100
+output_nodes = 10
 
 #定义学习率
 learning_rate = 0.3
 
 #创建神经网络对象
 n = NeuralNetWork(input_nodes, hidden_nodes, output_nodes, learning_rate)
-n.query([1, 2, 1])
+
+#导入训练集
+training_data_file = open("E:/mnist_train_100.csv", 'r')
+training_data_list = training_data_file.readlines()
+training_data_file.close()
+
+#训练测试集
+for record in training_data_list:
+    all_values = record.split(',')
+    inputs = (numpy.asfarray(all_values[1:])/255.0 * 0.99) + 0.01
+    targets = numpy.zeros(output_nodes) + 0.01
+    targets[int(all_values[0])] = 0.99
+    n.train(inputs, targets)
+
+#测试测试集
+
+#导入测试集
+test_data_file = open("E:/mnist_test_10.csv", 'r')
+test_data_list = test_data_file.readlines()
+test_data_file.close()
+
+#测试数据
+scorecard = []
+
+for record in test_data_list:
+    all_values = record.split(',')
+    #正确标签
+    correct_label = int(all_values[0])
+    print(correct_label, "correct_answer")
+    #输入归一化
+    inputs = (numpy.asfarray(all_values[1:])/255.0 * 0.99) + 0.01
+    #查询网络
+    outputs = n.query(inputs)
+    #输出矩阵最大的值就是结果
+    label = numpy.argmax(outputs)
+    print(label, "network_answer")
+
+    #匹配正确就加1
+    if(label == correct_label):
+        scorecard.append(1)
+    else:
+        scorecard.append(0)
+
+#输出最后正确率
+scorecard_array = numpy.asarray(scorecard)
+print(scorecard_array.sum() / scorecard_array.size)
+
+
+print('a')
